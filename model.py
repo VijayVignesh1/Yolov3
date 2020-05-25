0ad32d4242e9e706a74aea2a731e5a427a308c65
unknown <vijaydhonindia@gmail.com>
2020-05-25 21:23:23 +0530

    First Commit

---
+++ model.py
@@ -0,0 +1,172 @@
+import torch
+from util import *
+def build_net(cfg):
+    data=open(cfg,'r')
+    count=0
+    lines=data.readlines()
+    # print(lines)
+    # exit(0)
+    layer=0
+    modules=torch.nn.ModuleList()
+    output_filters=[3]
+    for i in range(len(lines)):
+        # print(i)
+        # print(lines[i])
+
+
+        if lines[i][1:-2]=="convolutional":
+            # print(lines[i])
+            # print("CONV")
+            i+=1
+            temp={}
+            seq=torch.nn.Sequential()
+            while lines[i]!="\n":
+                # print(lines[i].split("=")[1].rstrip("\n"))
+                key,value=lines[i].split("=")[0],lines[i].split("=")[1].rstrip("\n")
+                key,value=key.strip(),value.strip()
+                temp[key]=value
+                # print(lines[i])
+                i+=1
+            # print(temp)
+            if int(temp['pad']):
+                pad=(int(temp["size"])-1)//2
+            else:
+                pad=0
+            conv=torch.nn.Conv2d(output_filters[-1],int(temp["filters"]),int(temp["size"]),int(temp["stride"]),pad)
+            seq.add_module("conv_%d"%(layer),conv)
+            try:
+                if temp["batch_normalize"]:
+                    norm=torch.nn.BatchNorm2d(int(temp['filters']))
+                    seq.add_module("norm_%d"%(layer),norm)
+            except:
+                pass
+            if temp['activation']=="leaky":
+                act=torch.nn.LeakyReLU(0.1,inplace=True)
+                seq.add_module("act_%d"%(layer),act)
+            # seq=torch.nn.Sequential(conv,norm,act)
+            layer+=1
+            # print(modules)
+            output_filters.append(int(temp['filters']))
+            modules.append(seq)
+
+        elif lines[i][1:-2]=="upsample":
+            i+=1
+            temp={}
+            seq=torch.nn.Sequential()
+            while lines[i]!="\n":
+                key,value=lines[i].split("=")[0],lines[i].split("=")[1].rstrip("\n")
+                key,value=key.strip(),value.strip()
+                temp[key]=value
+                i+=1            
+            up=torch.nn.Upsample(scale_factor=2,mode="bilinear")
+            seq.add_module("up_%d"%(layer),up)
+            output_filters.append(output_filters[-1])
+            modules.append(seq)
+            layer+=1
+
+        elif lines[i][1:-2]=="route":
+            i+=1
+            temp={}
+            seq=torch.nn.Sequential()
+            while lines[i]!='\n':
+                key,value=lines[i].split("=")[0],lines[i].split("=")[1].rstrip("\n")
+                key,value=key.strip(),value.strip()
+                temp[key]=value
+                i+=1
+            if "," in temp['layers']:
+                start,end=temp['layers'].split(',')
+            else:
+                start=temp['layers']
+                end=0
+            start=int(start)
+            end=int(end)    
+            # print(start,end)  
+            if start>0:
+                start-=layer
+            if end>0:
+                end-=layer
+            route=EmptyLayer(is_route=True,start=start,end=end)
+            seq.add_module("route_%d"%(layer),route)
+            # print(start,end,layer+start)
+            # print(len(output_filters))
+            filters=output_filters[layer + start + 1]
+            print(layer,filters,start,end)
+            if end<0:
+                # assert output_filters[-1]==output_filters[skip]
+                # print(filters,output_filters[layer + end])
+                filters+=output_filters[layer + end + 1]
+            output_filters.append(filters)
+            modules.append(seq)
+            layer+=1
+
+        elif lines[i][1:-2]=='shortcut':
+            i+=1
+            temp={}
+            seq=torch.nn.Sequential()
+            while lines[i]!="\n":
+                key,value=lines[i].split("=")[0],lines[i].split("=")[1].rstrip("\n")
+                key,value=key.strip(),value.strip()
+                temp[key]=value
+                i+=1
+            # shortcut=EmptyLayer()
+            skip=int(temp['from'])
+            if skip>0:
+                skip-=layer
+            assert output_filters[-1]==output_filters[skip]
+            output_filters.append(output_filters[-1])  
+            shortcut=EmptyLayer(is_shortcut=True,skip=skip)    
+            seq.add_module("shortcut_%d"%(layer),shortcut)
+            modules.append(seq)
+            layer+=1
+
+        elif lines[i][1:-2]=="yolo":
+            i+=1
+            temp={}
+            seq=torch.nn.Sequential()
+            while lines[i]!="\n":
+                key,value=lines[i].split("=")[0],lines[i].split("=")[1].rstrip("\n")
+                key,value=key.strip(),value.strip()
+                temp[key]=value
+                i+=1
+            # print(temp)
+            masks=temp["mask"].split(',')
+            masks=[int(i) for i in masks]
+            anchors=temp['anchors'].split(",")
+            anchors=[(int(anchors[i]),int(anchors[i+1])) for i in range(0,len(anchors),2)]
+            anchors=[anchors[i] for i in masks]
+            detection=DetectionLayer(anchors)
+            seq.add_module("detection_%d"%(layer),detection)
+            output_filters.append(output_filters[-1])
+            modules.append(seq)
+            layer+=1
+
+        elif lines[i][1:-2]=="net":
+            # print("NET")
+            i+=1
+            net_info={}
+            while lines[i]!="\n":
+                if lines[i][0]=="#":
+                    i+=1
+                    continue
+                # print(lines[i])
+                key,value=lines[i].split("=")[0],lines[i].split("=")[1].rstrip("\n")
+                key,value=key.strip(),value.strip()
+                try:
+                    if "." in value:
+                        net_info[key]=float(value)
+                    else:
+                        net_info[key]=int(value)
+                except:
+                    pass
+                i+=1    
+        # modules.append(seq)
+        # layer+=1
+
+        # if count==25:
+        #     break
+        # count+=1
+    # print(modules)
+    # print(len(output_filters))
+    print("ad",output_filters[:84])
+    # print(net_info)
+    return net_info,modules
---
+++ yolo.py
@@ -0,0 +1,79 @@
+from model import *
+import torch
+net_info,modules=build_net('cfg/yolov3.cfg')
+# print(b[4][0].skip)
+# print(len(b))
+        
+# for i in range(106):
+#     if "Empty" in str(b[i][0]):
+#         print(i)
+#         exit(0)
+class DarkNet(torch.nn.Module):
+    def __init__(self):
+        super(DarkNet, self).__init__()
+        # self.blocks = parse_cfg(cfgfile)
+        self.net_info, self.module_list = build_net('yolov3.cfg')
+        self.cuda="cuda"
+    def forward(self,x):
+        outputs={}
+        write=0
+        # detections=""
+        # global detections
+        # detect=[]
+        for i in range(len(self.module_list)):
+            # print(self.module_list[i])
+            if "conv" in str(self.module_list[i]) or "up" in str(self.module_list[i]):
+                x=self.module_list[i](x)
+            elif "route" in str(self.module_list[i]):
+                assert self.module_list[i][0].is_route==True
+                start=self.module_list[i][0].start
+                end=self.module_list[i][0].end
+                # print("asa",start)
+                if end==0:
+                    x=outputs[i+(start)]
+                    # print(x.shape)
+                else:
+                    map1=outputs[i+(start)]
+                    map2=outputs[i+(end)]
+                    x=torch.cat((map1,map2),1)
+            elif "shortcut" in str(self.module_list[i]):
+                assert self.module_list[i][0].is_shortcut==True
+                skip=self.module_list[i][0].skip
+                # print(outputs[i-1].shape,outputs[i+skip+1].shape)
+                # print(skip,i)
+                x=outputs[i-1]+outputs[i+(skip)]
+            elif "detection" in str(self.module_list[i]):
+                anchors=self.module_list[i][0].anchors
+                inp_dim=int(self.net_info["height"])
+                num_classes=80
+                x=x.data
+                x=predict_transform(x,inp_dim,anchors,num_classes,True)
+                if not write:
+                    # global detections
+                    detections=x
+                    write=1
+                    # detect.append(detections)
+                    # print("deee",detect)
+                else:
+                    # global detections
+                    detections=torch.cat((detections,x),1)
+                    # detect[0]=detections
+            outputs[i]=x
+            # print(i,x.shape)
+        # global detections
+        return detections
+
+def get_test_input():
+    img = cv2.imread("dog-cycle-car.png")
+    img = cv2.resize(img, (416,416))          #Resize to the input dimension
+    img_ =  img[:,:,::-1].transpose((2,0,1))  # BGR -> RGB | H X W C -> C X H X W 
+    img_ = img_[np.newaxis,:,:,:]/255.0       #Add a channel at 0 (for batch) | Normalise
+    img_ = torch.from_numpy(img_).float()     #Convert to float
+    img_ = Variable(img_)                     # Convert to Variable
+    return img_
+
+model = DarkNet()
+model=model.to("cuda")
+inp = get_test_input()
+pred = model(inp.cuda())
+print (pred.shape)
